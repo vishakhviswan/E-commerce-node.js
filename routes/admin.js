e67f36300e81c8b2b15877a232586d4d30734c30
vishakhviswan <vishakhviswan1995@gmail.com>
2021-07-08 22:28:58 +0530

    mongodb connected and checked

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var productHelper=require('../helpers/product-helpers')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -34,7 +35,17 @@ router.get('/add-product',(req,res)=>{
   res.render('admin/add-product')
 })
 router.post('/add-product',(req,res)=>{
-  console.log(req.body);
-  console.log(req.files.Image)
+
+  productHelper.addProduct(req.body,(id)=>{
+    let image=req.files.Image
+    image.mv('./public/product-images/'+id+".jpg",(err)=>{
+      if(!err){
+        res.render("admin/add-product")
+      }else{
+        console.log(err);
+      }
+    })
+    res.render("admin/add-product")
+  })
 })
 module.exports = router;
